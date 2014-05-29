36bce1b8c523ee6112b1c2d14a75968a46b04fb2
schaef <martinschaef@gmail.com>
2014-05-29 13:10:01 -0700

    Initial commit.

---
+++ jar2bpl/src/org/joogie/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.joogie;
+
+/**
+ * @author schaef
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
