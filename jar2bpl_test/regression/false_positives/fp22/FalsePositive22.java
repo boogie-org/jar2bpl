5e44fb961b16ac8b0721f26a6c915a70aff386d1
schaef <martinschaef@gmail.com>
2014-09-19 20:57:18 -0700

    two more false positive test cases

---
+++ jar2bpl_test/regression/false_positives/fp22/FalsePositive22.java
@@ -0,0 +1,237 @@
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Calendar;
+import java.util.Map;
+import java.util.Properties;
+
+
+
+public class FalsePositive22 {
+
+	/*
+	 * From package org.apache.jmeter.util;
+	 * In file: BSFJavaScriptEngine.java
+	line 153
+	 */
+	public class BSFException extends Exception {}
+	
+	public class Context {
+
+		public void setOptimizationLevel(int i) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void setGeneratingDebug(boolean b) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public Object evaluateString(Object global, String scriptText,
+				String source, int lineNo, Object object) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		
+	}
+
+	private Object global;
+	
+    public Object eval(String source, int lineNo, int columnNo, Object oscript)
+            throws BSFException {
+
+            String scriptText = oscript.toString();
+            Object retval = null;
+            Context cx;
+
+            try {
+                cx = enter();
+
+                cx.setOptimizationLevel(-1);
+                cx.setGeneratingDebug(false);
+//                cx.setGeneratingSource(false);
+//                cx.setOptimizationLevel(0);
+//                cx.setDebugger(null, null);
+
+                retval = cx.evaluateString(global, scriptText,
+                                           source, lineNo,
+                                           null);
+
+                if (retval instanceof String)
+                    retval = unwrap();
+
+            }
+            catch (Throwable t) { // includes JavaScriptException, rethrows Errors
+                handleError(t);
+            }
+            finally {
+            	FalsePositive22.exit();
+            }
+            return retval;
+        }
+
+	private static void exit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void handleError(Throwable t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Object unwrap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Context enter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+/*=====================================================
+ * 	package org.apache.tools.ant.taskdefs.optional;
+ * In file: EchoProperties.java
+	line 312
+	line 360
+
+ */
+	private boolean failonerror;
+	private File inFile;
+	
+	public class BuildException extends Exception {}
+	
+    public void execute() throws BuildException {
+ 
+       if (inFile != null) {
+            if (inFile.exists() && inFile.isDirectory()) {
+                String message = "srcfile is a directory!";
+                if (failonerror) {
+                    throw new BuildException();
+                } else {
+                    log(message);
+                }
+                return;
+            }
+
+            if (inFile.exists() && !inFile.canRead()) {
+                String message = "Can not read from the specified srcfile!";
+                if (failonerror) {
+                    throw new BuildException();
+                } else {
+                    log(message);
+                }
+                return;
+            }
+
+            FileInputStream in = null;
+            try {
+                in = new FileInputStream(inFile);
+                Properties props = new Properties();
+                props.load(in);
+                
+            } catch (FileNotFoundException fnfe) {
+                String message =
+                    "Could not find file " + inFile.getAbsolutePath();
+                if (failonerror) {
+                    throw new BuildException();
+                } else {
+                    log(message);
+                }
+                return;
+            } catch (IOException ioe) {
+                String message =
+                    "Could not read file " + inFile.getAbsolutePath();
+                if (failonerror) {
+                    throw new BuildException();
+                } else {
+                    log(message);
+                }
+                return;
+            } finally {
+                inFile.canExecute();
+            }
+        }
+    }
+
+    private void log(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+  
+  /* ===========================================================
+   * package org.apache.tools.ant.util;
+   * In file: DateUtils.java
+	line 197
+   */
+    public static int getPhaseOfMoon(Calendar cal) {
+        // CheckStyle:MagicNumber OFF
+        int dayOfTheYear = cal.get(Calendar.DAY_OF_YEAR);
+        int yearInMetonicCycle = ((cal.get(Calendar.YEAR) - 1900) % 19) + 1;
+        int epact = (11 * yearInMetonicCycle + 18) % 30;
+        if ((epact == 25 && yearInMetonicCycle > 11) || epact == 24) {
+            epact++;
+        }
+        return (((((dayOfTheYear + epact) * 6) + 11) % 177) / 22) & 7;
+        // CheckStyle:MagicNumber ON
+    }    
+    
+    
+    /* ===========================================================
+     * package org.apache.tools.ant.util;
+     * In file: DateUtils.java
+  	line 197
+     */
+    public class ZipFile {
+	    private String archiveName;
+		private String encoding;
+		private Object zipEncoding;
+		private boolean useUnicodeExtraFields;
+		private RandomAccessFile archive;
+		private boolean closed;
+
+		public ZipFile(final File f, final String encoding, final boolean useUnicodeExtraFields)
+	            throws IOException {
+            this.archiveName = f.getAbsolutePath();
+            this.encoding = encoding;
+//            this.zipEncoding = ZipEncodingHelper.getZipEncoding(encoding);
+            this.useUnicodeExtraFields = useUnicodeExtraFields;
+            archive = new RandomAccessFile(f, "r");
+            boolean success = false;
+            try {
+                final Map<Object, Object> entriesWithoutUTF8Flag =
+                    populateFromCentralDirectory();
+                resolveLocalFileHeaderData(entriesWithoutUTF8Flag);
+                success = true;
+            } finally {
+                if (!success) {
+                    try {
+                        closed = true;
+                        archive.close();
+                    } catch (final IOException e2) {
+                        // swallow, throw the original exception instead
+                    }
+                }
+            }
+        }
+
+		private Map<Object, Object> populateFromCentralDirectory() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		private void resolveLocalFileHeaderData(
+				Map<Object, Object> entriesWithoutUTF8Flag) {
+			// TODO Auto-generated method stub
+			
+		}    
+   }
+}
+
